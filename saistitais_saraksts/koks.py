fcbbbad454a43fb59e6d56f34c8cd58a08a59ba4
martabritala <53839047+martabritala@users.noreply.github.com>
2025-01-16 16:48:50 +0200

    pievienota meklēšana, kārtošana

--- saistitais_saraksts/koks.py
+++ saistitais_saraksts/koks.py
@@ -57,8 +57,43 @@ class Koks:
         self.read_ja_ir(elements.bigger)
         return
 
+    def sort(self):
+        if self.sakne == None:
+            print("Nav elementu kokā")
+            return
+        sakums = self.sakne
+        self.read_mazakais(sakums)
 
+    def read_mazakais(self, mazakais):
+        if mazakais.smaller:
+            self.read_mazakais(mazakais.smaller)
+        mazakais.read()
+        if mazakais.bigger:
+            self.read_mazakais(mazakais.bigger)
+    
+    def search(self, meklejamais):
+        limenis, vecaks, skaits = self.parbauda_vienu(meklejamais, self.sakne, 0)
+        if limenis == -1:
+            print(f"Neeksitē elements, tika veiktas {skaits} pārbaudes")
+            return
+        if limenis == 0:
+            print(f"Elements ir koka sakne, tika veiktas {skaits} pārbaudes")
+            return
+        print(f"Elementa līmenis ir {limenis}, tā vecāks ir {vecaks}, tika veiktas {skaits} pārbaudes")
+        return
 
+    def parbauda_vienu(self, meklejamais, elements:Node, skaits):
+        skaits += 1
+        if meklejamais == elements.info:
+            vecaks = None
+            if elements.parent:
+                vecaks = elements.parent.info
+            return elements.level, vecaks, skaits
+        if elements.smaller and elements.info > meklejamais:
+            return self.parbauda_vienu(meklejamais, elements.smaller, skaits)
+        if elements.bigger and elements.info < meklejamais:
+            return self.parbauda_vienu(meklejamais, elements.bigger, skaits)
+        return -1, None, skaits
 
 
 
@@ -78,4 +113,5 @@ koks.add(7)
 koks.add(3)
 koks.add(2)
 koks.add(18)
-koks.read()
+koks.sort()
+koks.search(105)
